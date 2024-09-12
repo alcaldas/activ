af19e6ab555681db8cc3270e018e5ab729206194
alcaldas <al.caldas@duocuc.cl>
2024-09-12 17:09:24 -0300

    v2

--- src/app/paginas/home/home.page.ts
+++ src/app/paginas/home/home.page.ts
@@ -6,7 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario:string = '';
+  precioEntrada: number= 30000;
+  cantidadEntradas: number= 1;
+  totalPagar: number=0;
+  compraExitosa: boolean = false;
+  mensajeExito:string= '';
 
   constructor() {}
+  ngOnInit(){
+    this.usuario = localStorage.getItem('user') || 'Invitado'; 
+    this.calcularTotal();
+  }
 
+  calcularTotal(){
+    this.totalPagar = this.precioEntrada * this.cantidadEntradas;
+  }
+  comprarEntradas(){
+    this.calcularTotal();
+
+    this.compraExitosa = true;
+    this.mensajeExito = `Has comprado exitosamente una entrada`;
+
+  }
+  
+
+
+  
 }
--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -9,20 +9,34 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
   cliente={user:'',password:''}
   errorMessage:string='';
+  Message:string='';
   constructor(private router:Router) { }
 
-  ngOnInit() {
-  
-  }
-  onSubmit():Boolean {
-    if(this.cliente.user === 'admin'&& this.cliente.password === 'admin'){
-      this.router.navigate(['/home'])
-      return true;
-    }
-    else{
-      this.errorMessage="usuario o contraseña incorrecta"
-    }
-    return false;
+  ngOnInit() {}
 
+  onSubmit(): boolean {
+    //Verificar valores
+    console.log('Usuario ingresado: ', this.cliente.user);
+    console.log('Contraseña ingresada: ', this.cliente.password)
+
+
+    // Validar si los campos están vacíos
+    if (!this.cliente.user || !this.cliente.password) {
+      this.errorMessage = "Todos los campos son obligatorios";
+      return false;
+    }
+
+    //Validación de Usuario
+     if (this.cliente.user === 'admin' && this.cliente.password === 'admin') {
+      //Almacenar usuario en local storage
+      this.errorMessage = ''; // Limpiar mensaje de error
+      localStorage.setItem('usuario', this.cliente.user);
+      localStorage.setItem('token','some-auth-token');
+      this.router.navigate(['/home']);
+      return true;
+    } else {
+      this.errorMessage = "Usuario o contraseña incorrecta";
+      return false;
+    }
   }
 }
